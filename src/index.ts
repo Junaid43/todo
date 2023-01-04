91e17f142f36aeefc8483163054f6583e28f9641
Muhammad Junaid Tariq <muhammad.junaid434@gmail.com>
2023-01-04 21:39:57 +0500

    todo program

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import inquirer from 'inquirer';
+import { exit } from 'process';
+
+
+
+async function startCalculationAgain(){
+
+
+let todolist:string[] = [];
+
+
+
+let addSomething = await inquirer.prompt({
+    type:"confirm",
+    name:"add",
+    message:"Do you want to add something in your todo list",
+    default:false
+
+})
+
+    if (addSomething.add === true){
+
+        do {
+            let addTodo = await inquirer.prompt({
+                type:"input",
+                name:"addValue",
+                message:"Add value to your todo list"
+    
+            });
+    
+            let valTodo:string = addTodo.addValue;
+    
+            todolist.push(valTodo);
+
+            var playAgain = await inquirer.prompt({
+                type:"input",
+                name:"restart",
+                message:"Do you want add more ? Press Yes or No"
+    
+            });
+
+        } while (playAgain.restart == 'y' || playAgain.restart == 'Y' || playAgain.restart == 'YES' || playAgain.restart == 'Yes' || playAgain.restart == 'yes');
+        
+
+    }
+
+
+    if(todolist.length > 0){
+        todolist.forEach(todo =>{
+            console.log("Your Todo", todo);
+        });
+
+    }
+
+    else{
+        console.log("No item in your todo list");
+    }
+
+
+}
+
+
+startCalculationAgain();
